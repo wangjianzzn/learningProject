93a88ee28e9f2278e4e58fac84e825b37eb19cdc
王尼玛啊丶 <9530100+wangjianzzn@user.noreply.gitee.com>
2021-08-18 11:24:02 +0800

    0818

---
+++ day18/0818/src/test/TestFileWriter.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TestFileWriter {
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter("/Users/charlie/Desktop/test/aaa.txt",true);
+        writer.write("abccc");
+        writer.flush();
+    }
+}
